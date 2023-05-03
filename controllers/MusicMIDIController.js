02b106b3f7f86ad21a8f6d020078e6913b868dd3
Kevin-Allain <kevallain@gmail.com>
2023-05-03 12:22:56 +0100

    Returning empty data when there is no match

--- controllers/MusicMIDIController.js
+++ controllers/MusicMIDIController.js
@@ -48,73 +48,78 @@ module.exports.getMusicMIDI = async (req, res) => {
 
 
 
-  module.exports.getMatchLevenshteinDistance  =async (req,res) => {
+module.exports.getMatchLevenshteinDistance = async (req, res) => {
     console.log("---module.exports.getMatchLevenshteinDistance--- req.headers:", JSON.stringify(req.headers));
-    console.log("req.query: ",JSON.stringify(req.query));
-    console.log("req.body: ",JSON.stringify(req.body));
-    console.log("req.params: ",JSON.stringify(req.params));
+    console.log("req.query: ", JSON.stringify(req.query));
+    console.log("req.body: ", JSON.stringify(req.body));
+    console.log("req.params: ", JSON.stringify(req.params));
 
     // loading the entirety of the database will be a massive problem... 
     // we have to consider clever approaches to do so
     const arrayStrNotes = req.query.stringNotes.split('-')
-    const arrayNotes = arrayStrNotes.map( a => parseInt(a))
+    const arrayNotes = arrayStrNotes.map(a => parseInt(a))
     const firstNote = arrayNotes[0];
     const lengthSearch = arrayNotes.length;
-    console.log("firstNote: ",firstNote,", lengthSearch: ",lengthSearch);
+    console.log("firstNote: ", firstNote, ", lengthSearch: ", lengthSearch);
 
-      // First approach is imperfect, but will somehow be something...
-      // get all notes matching the first one 
-      // and then return all the following notes according to m_id
-      MusicMIDIModel.find({ pitch: firstNote })
-          .then(data => {
-              console.log("Searched successfully.")
-              console.log("data[0]: ", data[0]);
-              console.log("====")
-              // then find back other matches...
-              // get all the recordings, tracks, and matching m_id to push through
-              const arr_m_id = data.map(a => a.m_id);
-              const arr_recording = data.map(a => a.recording);
-              const uniqueIds = data.map(a => a._id);
-              console.log("uniqueIds[0]: ",uniqueIds[0]);
-              const uniqueStrIds = data.map( a => a._id.toString());
-              console.log("uniqueStrIds[0]: ",uniqueStrIds[0]);
-              // myObjectId = ObjectId("507c7f79bcf86cd7994f6c0e")
+    // First approach is imperfect, but will somehow be something...
+    // get all notes matching the first one 
+    // and then return all the following notes according to m_id
+    MusicMIDIModel.find({ pitch: firstNote })
+        .then(data => {
+            console.log("Searched successfully.")
+            console.log("data[0]: ", data[0]);
+            console.log("====")
+            // then find back other matches...
+            // get all the recordings, tracks, and matching m_id to push through
+            const arr_m_id = data.map(a => a.m_id);
+            const arr_recording = data.map(a => a.recording);
+            const uniqueIds = data.map(a => a._id);
+            console.log("uniqueIds[0]: ", uniqueIds[0]);
+            const uniqueStrIds = data.map(a => a._id.toString());
+            console.log("uniqueStrIds[0]: ", uniqueStrIds[0]);
+            // myObjectId = ObjectId("507c7f79bcf86cd7994f6c0e")
 
-              const query = {
-                  $or: data.map(({ recording, m_id }) => {
-                      const minMId = m_id;
-                      const maxMId = m_id + lengthSearch;
-                      return {
-                          recording,
-                          m_id: { $lte: maxMId, $gte: minMId }
-                      };
-                  })
-              };
+            // If there is no match, then return data already?
+            if (data[0] === undefined) {
+                res.send(data);
+            } else {
 
-              MusicMIDIModel.find(query)
-                  .lean()
-                  .then(d => {
-                    //   console.log(d[0])
-                      console.log("d[0]._id.toString(): ", d[0]._id.toString())
-                      console.log("uniqueStrIds[0]: ",uniqueStrIds[0])
+                const query = {
+                    $or: data.map(({ recording, m_id }) => {
+                        const minMId = m_id;
+                        const maxMId = m_id + lengthSearch;
+                        return {
+                            recording,
+                            m_id: { $lte: maxMId, $gte: minMId }
+                        };
+                    })
+                };
 
-                      d = d.sort( (a,b) => a.recording-b.recording || a.m_id-b.m_id )
+                MusicMIDIModel.find(query)
+                    .lean()
+                    .then(d => {
+                        //   console.log(d[0])
+                        console.log("d[0]._id.toString(): ", d[0]._id.toString())
+                        console.log("uniqueStrIds[0]: ", uniqueStrIds[0])
 
-                      // identify where the sequence started
-                      d.forEach(a =>
-                          a.startSequence = (uniqueStrIds.findIndex(b => b === a._id.toString()) !== -1)? true : false
-                      );
-                      //   for( let k in d){ if ( uniqueStrIds.findIndex(b => ) ) }
-                      console.log("query passed.")
-                      console.log("d[0]: ", d[0])
-                      console.log("d[0].forcingTest: ",d[0].forcingTest)
-                      console.log("d[0].startSequence: ",d[0].startSequence)
-                      res.send(d);
-                  })
+                        d = d.sort((a, b) => a.recording - b.recording || a.m_id - b.m_id)
 
-          })
-          .catch(error => { res.status(500).json(error); })
+                        // identify where the sequence started
+                        d.forEach(a =>
+                            a.startSequence = (uniqueStrIds.findIndex(b => b === a._id.toString()) !== -1) ? true : false
+                        );
+                        //   for( let k in d){ if ( uniqueStrIds.findIndex(b => ) ) }
+                        console.log("query passed.")
+                        console.log("d[0]: ", d[0])
+                        console.log("d[0].forcingTest: ", d[0].forcingTest)
+                        console.log("d[0].startSequence: ", d[0].startSequence)
+                        res.send(d);
+                    })
+            }
+        })
+        .catch(error => { res.status(500).json(error); })
 
 
     // res.send("TODO")
-  }
+}
