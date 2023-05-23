7e26c1d419ffefbb0194f39121926af3c29585d9
Kevin Allain <kevallain@gmail.com>
2023-05-23 15:46:08 +0100

    Changes to set an index to the search, this makes occurences of patterns in a track unique samples. Value is index 0 for other objects

--- controllers/AnnotationController.js
+++ controllers/AnnotationController.js
@@ -2,9 +2,9 @@ const AnnotationModel = require("../models/AnnotationModel");
 
 module.exports.addAnnotation = async (req, res) => {
     console.log("---module.exports.addAnnotation--- req.headers:", req.headers);
-    const { type, info, annotationInput, user, privacy } = req.body;
+    const { type, info, annotationInput, author, privacy } = req.body;
 
-    AnnotationModel.create({ type: type, info: info, annotationInput: annotationInput, user: user, privacy: privacy })
+    AnnotationModel.create({ type: type, info: info, annotationInput: annotationInput, author: author, privacy: privacy })
         .then((data) => {
             console.log("Added successfully");
             console.log(data);
@@ -18,9 +18,10 @@ module.exports.addAnnotation = async (req, res) => {
 
 module.exports.getAnnotations = async (req, res) => {
     console.log("---module.exports.getAnnotations--- req.query:", req.query);
-    const { type, info } = req.query;
+    const { type, info, index } = req.query;
 
-    AnnotationModel.find({ type: type, info: info })
+    // TODO assess with tests if approach correct
+    AnnotationModel.find({ type: type, info: info, index:index })
         .then(data => {
             console.log("Searched successfully.")
             console.log("data.length: ", data.length);
@@ -30,33 +31,37 @@ module.exports.getAnnotations = async (req, res) => {
 };
 
 module.exports.deleteAnnotation = async (req, res) => {
-    console.log("---module.exports.deleteAnnotation--- req.query:", req.query,", req.body: ",req.body);
-    
+    console.log("---module.exports.deleteAnnotation--- req.query:", req.query, ", req.body: ", req.body);
+
     const { _id } = req.body;
 
     AnnotationModel.findByIdAndDelete(_id)
-      .then(() => {
-        console.log("Deleted successfully");
-        res.send("Deleted successfully");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        .then(() => {
+            console.log("Deleted successfully");
+            res.send("Deleted successfully");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 }
 
-module.exports.updateAnnotation = async (req, res) => {  
-    console.log( "module.exports.updateAnnotation. req.body: ",req.body);
+module.exports.updateAnnotation = async (req, res) => {
+    console.log("module.exports.updateAnnotation. req.body: ", req.body);
     const { _id, annotationInput, userId } = req.body;
-  
+
+    // TODO change, this should make a modification on collaborators
     AnnotationModel.findByIdAndUpdate(_id, {
-      $set: { annotationInput: annotationInput, user: userId },
+        $set: { annotationInput: annotationInput }, //, author: userId },
+        $push: { collaborators: userId },
     })
-      .then(() => {
-        console.log("Updated successfully");
-        res.send("Updated successfully");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  
+        .then(() => {
+            console.log("Updated successfully");
+            res.send("Updated successfully");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+
+// comments for annotations?
--- models/AnnotationModel.js
+++ models/AnnotationModel.js
@@ -9,27 +9,34 @@ const annotationSchema = new mongoose.Schema({
         type:String,
         require:true
     },
+    index:{
+        type: Number,
+        require:true
+    },
     annotationInput:{
         type:String,
         require:true
     },
-    user:{
+    author:{
         type:String,
         require:true
-    },     
+    },
     privacy:{
         type:String,
         require:true
-    }, 
+    },
     starred_by:{
         type:[String],
         require:true
-    }
-    // TODO think about colaboration for annotations
-    // collaborators:{
-    //     type:[String],
-    //     require:true
-    // },
+    },
+    collaborators:{
+        type:[String],
+        require:true
+    },
+    comments:{
+        type:[String],
+        require:true
+    },
 })
 
 module.exports=mongoose.model('Annotation',annotationSchema)
